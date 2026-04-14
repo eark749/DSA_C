1c54c9f674f6f72865766c5382fdcbc1bcfe1183
vansh <vanshsoniofficial@gmail.com>
2026-04-14 15:14:31 +0530

    function declaring and defination

--- function.cpp
+++ function.cpp
@@ -4,13 +4,21 @@
 using std::cout;
 using std::cin;
 
+double power(double,int);
+
+
 int main() {
     int base, expo;
     cout<<"Enter base: ";
     cin>>base;
     cout<<"Enter exponent: ";
     cin>>expo;
+    double tpower = power(base, expo);
     //cout << pow(base,expo);
-    double power = pow(base,expo);
-    cout << power<<" this is the power \n";
+    //double power = pow(base,expo);
+    cout << tpower<<" this is the power \n";
+}
+
+double power(double base, int exponent) {
+    return 0.0;
 }
