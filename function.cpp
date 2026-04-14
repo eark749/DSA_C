c6e0be77a218ceef92b52d56ba26c4df6a198557
vansh <vanshsoniofficial@gmail.com>
2026-04-14 14:35:46 +0530

    math function, use of cmath lib

---
+++ function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using std::cout;
+using std::cin;
+
+int main() {
+    int base, expo;
+    cout<<"Enter base: ";
+    cin>>base;
+    cout<<"Enter exponent: ";
+    cin>>expo;
+    cout << pow(base,expo);
+}
